5074fe1ecb33398905388bce56a17fb2da48e517
ExploHash <jbtwaalf@gmail.com>
2025-06-29 11:01:11 +0200

    fix: correctly parse string as number

--- src/recordings/recordings.controller.ts
+++ src/recordings/recordings.controller.ts
@@ -26,7 +26,7 @@ export class RecordingsController {
     @UploadedFile() file_enc: Express.Multer.File,
     @Body('transcript_enc') transcript_enc: string,
     @Body('transcriptFormatted_enc') transcriptFormatted_enc: string,
-    @Body('length') length: number,
+    @Body('length') length: string,
     @Param('entryId') entryId: string,
     @Param('journalId') journalId: string,
     @Request() request: { userId: string },
@@ -42,7 +42,7 @@ export class RecordingsController {
       entryId,
       journalId,
       request.userId,
-      length,
+      +length,
     );
   }
 
